9630748b995cf17f6fc8536317abc61a19320497
Rakibul71 <rakibulislam202019@gmail.com>
2021-08-10 22:12:29 +0600

    sidebar complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import "./app.css";
 
 function App() {
   return (
     <div>
       <Topbar></Topbar>
+      <div className="container">
+        <Sidebar></Sidebar>
+        <div className="others">Others</div>
+      </div>
     </div>
   );
 }
