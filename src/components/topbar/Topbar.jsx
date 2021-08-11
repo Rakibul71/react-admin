928de3d85c677f4e821861dc0ea9636ca244437f
Rakibul71 <rakibulislam202019@gmail.com>
2021-08-11 21:22:00 +0600

    mission completed

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./topbar.css";
-import { NotificationsNone,Language, Settings } from "@material-ui/icons";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
 
 const Topbar = () => {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">Lamaadmin</span>
+          <span className="logo">AdminPanel</span>
         </div>
 
         <div className="topRight">
@@ -24,8 +24,12 @@ const Topbar = () => {
           <div className="topbarIconContainer">
             <Settings />
           </div>
-          
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+
+          <img
+            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="topAvatar"
+          />
         </div>
       </div>
     </div>
